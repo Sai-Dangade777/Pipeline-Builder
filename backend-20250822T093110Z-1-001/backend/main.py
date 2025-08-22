f1378665bd9ef9e9310bc12709ca3a3db216fd7a
Sai-Dangade777 <sai3dangade@gmail.com>
2025-08-22 22:16:45 +0530

    minor fixes

--- backend-20250822T093110Z-1-001/backend/main.py
+++ backend-20250822T093110Z-1-001/backend/main.py
@@ -66,10 +66,4 @@ def parse_pipeline(
         'is_dag': is_dag(nodes, edges)
     }
 
-@app.route('/')
-def home():
-    return "Hello from Pipeline Builder!"
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
